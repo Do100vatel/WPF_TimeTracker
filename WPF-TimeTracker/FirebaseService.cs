710ebc5203878275053890bfd38011ab3e8754d9
Vlad <gamespeoople@gmail.com>
2024-07-12 10:44:02 +0300

    frst cm

---
+++ WPF-TimeTracker/FirebaseService.cs
@@ -0,0 +1,32 @@
+﻿using Firebase.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_TimeTracker
+{
+    public class FirebaseService
+    {
+        private readonly FirebaseClient _firebaseClient;
+
+        public FirebaseService()
+        {
+            _firebaseClient = new FirebaseClient("https://wpftimetracker.firebaseapp.com");
+        }
+
+        public async Task SaveTimer(TimerModel timer)
+        {
+            await _firebaseClient.Child("Timers").PostAsync(timer);
+        }
+
+        public async Task<List<TimerModel>> GetTimers()
+        {
+            return (await _firebaseClient.Child("Timers").OnceAsync<TimerModel>()).Select(item => item.Object).ToList();
+        }
+
+        // Другие методы для работы с Firebase
+    }
+
+}
---
+++ WPF-TimeTracker/TimerModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_TimeTracker
+{
+    public class TimerModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string CategoryId { get; set; }
+        // add properties
+
+    }
+
+    public class CategoryModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string ParentCategoryID { get; set; }// Для вложенных категорий
+        // Дополнительные свойства
+    }
+
+    public class TimeEntryModel
+    {
+        public string Id { get; set;}
+        public string TimerId{ get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Notes { get; set; }
+
+        // add properties
+    } 
+}
